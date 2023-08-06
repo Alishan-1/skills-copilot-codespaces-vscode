5bd3340c2ab2573212a1ccb232c30901b7ae1c5d
Ali Shan <43717880+Alishan-1@users.noreply.github.com>
2023-08-06 07:59:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function WriteAlphebets(){
+    var str = "";
+    str += "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+    return str;
+}
+function skillsMember(){
+    var str = "";
+    str += "HTML CSS JavaScript PHP";
+    return str; 
+}
